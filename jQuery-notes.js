ede6bd6bad56d29e7437a213d0443fc74667afbe
Andres <floresc.andres@intracitygeeks.org>
2018-05-12 14:29:41 +0000

    added the notes document

---
+++ jQuery-notes.js
@@ -0,0 +1,7 @@
+// Lesson 1 and 2
+
+jQuery is not a programming language, it is a library for Java Script
+jQuery allows for work between js and the DOM to be much easier, as well as working with events and animations
+
+jQuery also helps a lot by chaining methods/effects etc to elements in one line using periods in between. jQuery also has tons of pluggings as well
+
